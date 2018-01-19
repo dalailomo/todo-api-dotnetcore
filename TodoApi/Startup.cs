9e9c75d08abfe4c501887b0617ced9154f0d945b
Pedro Sánchez Casas <pedro.s.casas@tokiota.com>
2018-01-19 11:52:30 +0100

    Use SQLServer and add migrations

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -27,7 +27,9 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+            services.AddDbContext<TodoContext>(
+                opt => opt.UseSqlServer(Configuration.GetConnectionString("TodoListContext"))
+            );
             
             // cors for dev
             services.AddCors(options =>
